da4a1e438f9f4a2e9758b5dc67c5bf73ac2250d2
Oleg Kosmakov <1533952+kosmakoff@users.noreply.github.com>
2023-12-06 12:33:06 +0100

    Day 2

---
+++ Day02/Game.cs
@@ -0,0 +1,109 @@
+﻿namespace Day02;
+
+public class Game
+{
+    public int Id { get; }
+    public List<Poll> Polls { get; }
+
+    public Game(int id, List<Poll> polls)
+    {
+        Id = id;
+        Polls = polls;
+    }
+
+    public override string ToString()
+    {
+        return $"Game {Id}: {string.Join("; ", Polls)}";
+    }
+
+    public static Game Parse(string rawGame)
+    {
+        var rawGameSpan = rawGame.AsSpan();
+        var colonIndex = rawGameSpan.IndexOf(':');
+        var gameIdSpan = rawGameSpan[5..colonIndex];
+        var gameId = int.Parse(gameIdSpan);
+
+        var polls = new List<Poll>();
+        
+        var pollsSpan = rawGameSpan[(colonIndex + 1)..];
+        while (true)
+        {
+            var semicolonIndex = pollsSpan.IndexOf(';');
+
+            var pollSpan = semicolonIndex >= 0 ? pollsSpan[..semicolonIndex] : pollsSpan;
+            var poll = Poll.Parse(pollSpan);
+            polls.Add(poll);
+
+            pollsSpan = semicolonIndex >= 0 ? pollsSpan[(semicolonIndex + 1)..] : ReadOnlySpan<char>.Empty;
+
+            if (semicolonIndex == -1)
+            {
+                break;
+            }
+        }
+
+        return new Game(gameId, polls);
+    }
+
+    public class Poll
+    {
+        public int Red { get; }
+        public int Green { get; }
+        public int Blue { get; }
+
+        public override string ToString()
+        {
+            return $"{Red} red, {Green} green, {Blue} blue";
+        }
+
+        public Poll(int red, int green, int blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+
+        public static Poll Parse(ReadOnlySpan<char> pollSpan)
+        {
+            int red = 0, green = 0, blue = 0;
+            
+            while (true)
+            {
+                int comaIndex = pollSpan.IndexOf(',');
+
+                var colorSpan = comaIndex >= 0 ? pollSpan[..comaIndex].Trim() : pollSpan.Trim();
+                pollSpan = comaIndex >= 0 ? pollSpan[(comaIndex + 1)..] : ReadOnlySpan<char>.Empty;
+
+                if (colorSpan.IsEmpty)
+                {
+                    break;
+                }
+
+                var spaceIndex = colorSpan.IndexOf(' ');
+                var numberSpan = colorSpan[..spaceIndex];
+                var colorNameSpan = colorSpan[(spaceIndex + 1)..];
+                var number = int.Parse(numberSpan);
+
+                switch (colorNameSpan)
+                {
+                    case "red":
+                        red = number;
+                        break;
+                    case "green":
+                        green = number;
+                        break;
+                    case "blue":
+                        blue = number;
+                        break;
+                }
+                
+                if (comaIndex == -1)
+                {
+                    break;
+                }
+            }
+
+            return new Poll(red, green, blue);
+        }
+    }
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,49 @@
+﻿using static Common.Utils;
+
+namespace Day02;
+
+class Program
+{
+    private const int MaxRed = 12;
+    private const int MaxGreen = 13;
+    private const int MaxBlue = 14;
+    
+    static void Main()
+    {
+        PrintHeader("Day 02");
+
+        var input = File.ReadAllLines("Input.txt");
+
+        var answer1 = CalculateAnswer1(input);
+        var answer2 = CalculateAnswer2(input);
+
+        PrintAnswer("Answer 1", answer1);
+        PrintAnswer("Answer 2", answer2);
+    }
+
+    private static int CalculateAnswer1(string[] input)
+    {
+        return input
+            .Select(Game.Parse)
+            .Where(game => game.Polls.All(poll => poll is { Red: <= MaxRed, Green: <= MaxGreen, Blue: <= MaxBlue }))
+            .Sum(game => game.Id);
+    }
+
+    private static int CalculateAnswer2(string[] input)
+    {
+        return input
+            .Select(Game.Parse)
+            .Select(CalculatePower)
+            .Sum();
+
+        static int CalculatePower(Game game)
+        {
+            return game.Polls.Aggregate(
+                (Red: 0, Green: 0, Blue: 0),
+                (acc, poll) => (Math.Max(acc.Red, poll.Red),
+                    Math.Max(acc.Green, poll.Green),
+                    Math.Max(acc.Blue, poll.Blue)),
+                acc => acc.Red * acc.Green * acc.Blue);
+        }
+    }
+}
